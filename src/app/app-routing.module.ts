4cfd763118ee27618ccc9a24cda568bfb4184709
Gabriel Lavalle <gabrielecientista@gmail.com>
2019-11-27 11:40:08 -0200

    Primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { QuadrasComponent } from './quadras/quadras.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContatoComponent } from './contato/contato.component';
+import { LoginComponent } from './login/login.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 
 
 const routes: Routes = [
   {path: '',component: HomeComponent},
-  {path: 'quadras',component: QuadrasComponent}
+  {path: 'quadras',component: QuadrasComponent},
+  {path: 'cadastro',component: CadastroComponent},
+  {path:'faq',component: FaqComponent},
+  {path:'contato',component: ContatoComponent},
+  {path:'login',component: LoginComponent},
+  {path:'quem-somos',component: QuemSomosComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { QuadrasComponent } from './quadras/quadras.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContatoComponent } from './contato/contato.component';
+import { LoginComponent } from './login/login.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    QuadrasComponent
+    QuadrasComponent,
+    CadastroComponent,
+    FaqComponent,
+    ContatoComponent,
+    LoginComponent,
+    QuemSomosComponent
+   
   ],
   imports: [
     BrowserModule,
